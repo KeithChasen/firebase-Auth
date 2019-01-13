429515e25e8533e4b51f42d81736c1de18b3e237
keithchasenm <keithchasen89@gmail.com>
2019-01-13 21:03:13 +0200

    posts listing

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,8 @@
+//get data
+db.collection('posts').get().then(response => {
+    setupGuides(response.docs)
+})
+
 //listen for auth status changes
 auth.onAuthStateChanged(user => {
     if (user) {
--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+const postList = document.querySelector('.posts')
+
+const setupGuides = data => {
+    let html = '';
+    data.forEach(doc => {
+        const post = doc.data()
+        const li = `
+        <li>
+           <div class="collapsible-header grey lighten-4">${post.title}</div>
+           <div class="collapsible-body white">${post.content}</div>
+        </li>
+        `
+        html += li
+    })
+
+    postList.innerHTML = html
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     var modals = document.querySelectorAll('.modal')
     M.Modal.init(modals)
