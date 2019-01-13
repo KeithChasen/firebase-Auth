9a4aee94dcf43ab6acb7ec4a94999a141cfe6067
keithchasenm <keithchasen89@gmail.com>
2019-01-13 20:00:59 +0200

    login

--- js/auth.js
+++ js/auth.js
@@ -21,3 +21,18 @@ logout.addEventListener('click', (e) => {
             console.log("User's logged out")
         })
 })
+
+const loginForm = document.querySelector('#login-form')
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const email = loginForm['login-email'].value
+    const password = loginForm['login-password'].value
+
+    auth.signInWithEmailAndPassword(email, password)
+        .then(response => {
+            const modal = document.querySelector('#modal-login')
+            M.Modal.getInstance(modal).close()
+            loginForm.reset()
+        })
+})
