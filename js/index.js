4677cb3c97b69950a479e71b3f98da93a450ce28
keithchasenm <keithchasen89@gmail.com>
2019-01-14 14:27:26 +0200

    get rid of extra variable for li element

--- js/index.js
+++ js/index.js
@@ -18,13 +18,12 @@ const setupGuides = data => {
         let html = '';
         data.forEach(doc => {
             const post = doc.data()
-            const li = `
-        <li>
-           <div class="collapsible-header grey lighten-4">${post.title}</div>
-           <div class="collapsible-body white">${post.content}</div>
-        </li>
-        `
-            html += li
+            html += `
+                <li>
+                   <div class="collapsible-header grey lighten-4">${post.title}</div>
+                   <div class="collapsible-body white">${post.content}</div>
+                </li>
+                `
         })
 
         postList.innerHTML = html
